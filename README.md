2b9e577fe04772ab1c4ae2d0a8652fc9d21319da
SukanyaPradhan <71492912+SukanyaPradhan@users.noreply.github.com>
2020-11-16 17:20:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pendullum
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ Rope.js
@@ -0,0 +1,32 @@
+class rope{
+    constructor(bodyA, bodyB,OffsetX , OffsetY){
+        this.OffsetX=OffsetX
+        this.OffsetY=OffsetY
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            ponintB:{x:this.OffsetX,y:this.OffsetY}
+           
+        }
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+    display(){
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.rope.bodyB.position;
+        
+        stroke(255);
+        strokeWeight(4);
+
+        var Anchor1X=pointA.x
+        var Anchor1Y=pointA.y
+
+        var Anchor2X=pointB.x+this.OffsetX
+        var Anchor2Y=pointB.y+this.OffsetY
+
+
+        line(Anchor1X , Anchor1Y , Anchor2X , Anchor2Y);
+    }
+    
+}
